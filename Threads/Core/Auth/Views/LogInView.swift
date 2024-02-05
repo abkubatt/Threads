f0f9b3d6d8b971913e68678e7da8a4fe1c836e3a
abdulmajit <abkubatt@gmail.com>
2024-02-06 01:42:31 +0600

    configured folders

---
+++ Threads/Core/Auth/Views/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  Threads
+//
+//  Created by Abdulmajit Kubatbekov on 6/2/24.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
