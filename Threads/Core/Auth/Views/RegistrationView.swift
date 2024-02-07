bdac62ecf1ebac35306875ce52dedd711e17ff13
abdulmajit <abkubatt@gmail.com>
2024-02-07 18:42:48 +0600

    added Registration View

---
+++ Threads/Core/Auth/Views/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  Threads
+//
+//  Created by Abdulmajit Kubatbekov on 7/2/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
