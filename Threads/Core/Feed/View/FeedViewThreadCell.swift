f6e5f6bf1b51e7d52dcf0af099cc6ab78d22e586
abdulmajit <abkubatt@gmail.com>
2024-02-08 15:46:42 +0600

    added launch screen

---
+++ Threads/Core/Feed/View/FeedViewThreadCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewThreadCell.swift
+//  Threads
+//
+//  Created by Abdulmajit Kubatbekov on 8/2/24.
+//
+
+import SwiftUI
+
+struct FeedViewThreadCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedViewThreadCell()
+}
