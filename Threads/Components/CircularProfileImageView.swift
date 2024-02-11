936686ec0d34368aecda51f94ae4dbcf4eb4c96f
abdulmajit <abkubatt@gmail.com>
2024-02-11 20:39:17 +0600

    some changes

---
+++ Threads/Components/CircularProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileImageView.swift
+//  Threads
+//
+//  Created by Abdulmajit Kubatbekov on 11/2/24.
+//
+
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileImageView()
+}
---
+++ Threads/Components/UserCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserCell.swift
+//  Threads
+//
+//  Created by Abdulmajit Kubatbekov on 11/2/24.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserCell()
+}
