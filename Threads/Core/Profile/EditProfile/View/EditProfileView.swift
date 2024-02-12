009c119e7db69e3cb00cb578eed92ad1f4fed980
abdulmajit <abkubatt@gmail.com>
2024-02-12 14:13:12 +0600

    layouted profile view

---
+++ Threads/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Threads
+//
+//  Created by Abdulmajit Kubatbekov on 13/2/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
