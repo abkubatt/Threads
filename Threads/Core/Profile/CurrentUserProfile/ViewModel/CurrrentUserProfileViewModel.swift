4e53ec6b91bce310647a0d075ef2a0b14803134b
abdulmajit <abkubatt@gmail.com>
2024-02-20 21:05:46 +0600

    some change in editProfile

--- Threads/Core/Profile/CurrentUserProfile/ViewModel/CurrrentUserProfileViewModel.swift
+++ Threads/Core/Profile/CurrentUserProfile/ViewModel/CurrrentUserProfileViewModel.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 import Combine
+import PhotosUI
+import SwiftUI
 
 final class CurrentUserProfileViewModel: ObservableObject {
     @Published var currentUser: User?
+    @Published var selectedItem: PhotosPickerItem? {
+        didSet {
+            Task { await loadImage() }
+        }
+    }
+    @Published var profileImage: Image?
     private var cancellables = Set<AnyCancellable>()
     
     init() {
@@ -22,5 +30,12 @@ final class CurrentUserProfileViewModel: ObservableObject {
             print("user from combine: \(String(describing: user))")
         }
         .store(in: &cancellables)
+    }
+    
+    private func loadImage() async {
+        guard let item = selectedItem else { return }
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+        self.profileImage = Image(uiImage: uiImage)
     }
 }
